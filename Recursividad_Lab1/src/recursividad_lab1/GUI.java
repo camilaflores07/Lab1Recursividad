301bc83daf4202d95ad21807e6890147ef900b76
Chung-Un <chungunyum23@gmail.com>
2025-01-24 15:03:20 -0600

    ChungUn Cambios GUI

--- Recursividad_Lab1/src/recursividad_lab1/GUI.java
+++ Recursividad_Lab1/src/recursividad_lab1/GUI.java
@@ -6,6 +6,8 @@ package recursividad_lab1;
 
 import java.awt.BorderLayout;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -13,10 +15,17 @@ import javax.swing.*;
  * @author chung
  */
 public class GUI {
-   static JFrame frame = new JFrame();
+   static JFrame frame2 = new JFrame();
+   static JFrame frame1 = new JFrame();
    static JPanel panel1 = new JPanel();
    static JPanel panel2 = new JPanel();
-   static JButton btnBuscarAsientos = new JButton();
+   static JButton btnSalir = new JButton();
+   static JButton btnVender = new JButton();
+   static JButton btnCancelar = new JButton();
+   static JButton btnBuscarPasajero = new JButton();
+   static JButton btnImprimirPasajeros = new JButton();
+   static JButton btnReset = new JButton();
+   static JButton btnDespegar = new JButton();
    static JButton btnOk = new JButton();
    static JLabel labelNombre = new JLabel();
    static JTextField fieldNombre = new JTextField();
@@ -33,24 +42,52 @@ public class GUI {
    return btn;
    }
    
+   public static JFrame framePersonalizar ( JFrame frame){
    
-   public static void pantallaInicio(){
    frame.setTitle("Lab 1: Recursividad");
    frame.setSize(500,500);
    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   frame.setLayout(new BorderLayout());
+//   frame.setLayout(new BorderLayout());
+ 
+   return frame;
+   }
    
-   panel1 = new JPanel(new BoxLayout (panel1, BoxLayout.X_AXIS));
+   public static void pantallaInicio(){
+       frame2 = framePersonalizar(frame2);
+       panel2 = new JPanel(new GridLayout(5,1,10,10));
+       panel2.setBackground(Color.WHITE);
+       frame2.add(panel2);
+       
+       btnVender = btnPersonalizar(btnVender, "Buscar Asiento Vacio");
+       panel2.add(btnVender);
+       
+       btnCancelar = btnPersonalizar(btnCancelar, "Cancelar boleto");
+       panel2.add(btnCancelar);
+       
+       btnBuscarPasajero = btnPersonalizar(btnBuscarPasajero, "Buscar pasajero");
+       panel2.add(btnBuscarPasajero);
+       
+       btnImprimirPasajeros= btnPersonalizar(btnImprimirPasajeros, "Imprimir pasajeros");
+       panel2.add(btnImprimirPasajeros);
+       
+       btnDespegar= btnPersonalizar(btnDespegar,"Despegar");
+       panel2.add(btnDespegar);
+       
+       frame2.setVisible(true);
+       
+   
+   }
+   
+   public static void pantallaNombre(){
+   
+   
+   panel1 = new JPanel(new GridLayout(2,2,10,10));
    panel1.setBackground(Color.white);
-   frame.add(panel1, BorderLayout.NORTH);
-   
-   panel2.setBackground(Color.white);
-   frame.add(panel2, BorderLayout.SOUTH);
+   frame2.add(panel1);
    
    labelNombre.setText("Nombre: ");
    labelNombre.setForeground(Color.PINK);
-   labelNombre.setFont(new Font("Arial", Font.BOLD, 40));
-   labelNombre.setAlignmentX(Component.CENTER_ALIGNMENT);
+   labelNombre.setFont(new Font("Arial", Font.BOLD, 40));;
    panel1.add(labelNombre);
    
    fieldNombre.setText("");
@@ -65,7 +102,16 @@ public class GUI {
    btnOk = btnPersonalizar(btnOk,"Ok");
    panel1.add(btnOk);
    
-   frame.setVisible(true);
+   btnSalir = btnPersonalizar(btnSalir, "Cancelar");
+   
+   btnSalir.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        frame2.dispose();
+        }
+    });
+   
+   frame2.setVisible(true);
    
    }
     
