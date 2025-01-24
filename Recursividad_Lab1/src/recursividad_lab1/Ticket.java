a6b0d0a92d0a2c7c92d0d87bc92ed576a3883310
camilaflores07 <camila_maria@unitec.edu>
2025-01-24 13:51:59 -0600

    Clases de ticket y palindromoair

---
+++ Recursividad_Lab1/src/recursividad_lab1/Ticket.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad_lab1;
+
+/**
+ *
+ * @author 50494
+ */
+public class Ticket {
+    
+}
