6ab64afdf5a1f413ff51d1075a4ddd53efd91645
camilaflores07 <camila_maria@unitec.edu>
2025-01-24 14:25:22 -0600

    cambio2
    update 2

--- Recursividad_Lab1/src/PalindromoAir.java
+++ Recursividad_Lab1/src/PalindromoAir.java
@@ -1,3 +1,6 @@
+
+import recursividad_lab1.Ticket;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,42 @@
  *
  * @author 50494
  */
-public class PalindromoAir {
+public class PalindromoAir{
+    Ticket[] asientos = new Ticket[30];
+    final double TicketPrecio = 800;
     
+    public int firstAvailable(int i){
+        if (i >= asientos.length) 
+            return -1; 
+        if (asientos[i] == null) 
+            return i; 
+        return firstAvailable(i+1);
+    }
+    
+    public int searchPassenger(String name, int i){
+         if (i >= asientos.length) 
+            return -1; 
+         if (asientos[i] != null && asientos[i]) {
+            
+        }
+    }
+    
+    public boolean isPalindromo(String name){
+    }
+    
+    public void printPassengers(){
+    }
+    
+    public double income(){
+    }
+    
+    public void sellTicket(String name){
+    }
+    
+    public boolean cancelTicket(String name){
+    }
+    
+    public void dispatch(){
+    }
+   
 }
--- Recursividad_Lab1/src/recursividad_lab1/Ticket.java
+++ Recursividad_Lab1/src/recursividad_lab1/Ticket.java
@@ -9,5 +9,31 @@ package recursividad_lab1;
  * @author 50494
  */
 public class Ticket {
+    String NombrePasajero;
+    double totalpagar;
+
+    public Ticket(String NombrePasajero, double totalpagar) {
+        this.NombrePasajero = NombrePasajero;
+        this.totalpagar = totalpagar;
+    }
+
+    public String getNombrePasajero() {
+        return NombrePasajero;
+    }
+
+    public double getTotalpagar() {
+        return totalpagar;
+    }
     
+    public void print(){
+        System.out.println("El nombre del pasajero es: " + NombrePasajero);
+        System.out.println("El pasajero " + NombrePasajero + "debe de pagar: " + totalpagar);
+ } 
 }
+
+    
+
+  
+    
+ 
+
