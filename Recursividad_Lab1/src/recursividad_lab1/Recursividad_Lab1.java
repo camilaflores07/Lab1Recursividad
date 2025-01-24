fdec8f86bc6af48c274bcaeab60c18314fb33871
Chung-Un <chungunyum23@gmail.com>
2025-01-24 16:41:54 -0600

    Cambios

--- Recursividad_Lab1/src/recursividad_lab1/Recursividad_Lab1.java
+++ Recursividad_Lab1/src/recursividad_lab1/Recursividad_Lab1.java
@@ -4,6 +4,10 @@
  */
 package recursividad_lab1;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author chung
@@ -16,7 +20,46 @@ public class Recursividad_Lab1 {
     public static void main(String[] args) {
         // TODO code application logic here
         GUI.pantallaInicio();
-    }
+        PalindromoAir PalindromoAir = new PalindromoAir();
+        
+       
+        
+         GUI.btnImprimirPasajeros.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        GUI.frame2.dispose();
+        GUI.frameImprimir.setVisible(true);
+        PalindromoAir.printPassengers(0);
+        }
+        });
+        
+        GUI.btnVender.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        GUI.frame2.dispose();
+        GUI.frameVender.setVisible(true);
+        
+        int num = PalindromoAir.firstAvailable(0);
+        if(num != 1){
+            int opcion=0;
+        JOptionPane.showConfirmDialog(null, "El asiento " + num + " esta vacio. Desea venderlo?");
+            if(opcion == JOptionPane.YES_OPTION){
+            String nombre;
+            nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+            PalindromoAir.sellTicket(nombre,0);
+            GUI.frameVender.setVisible(false);
+            GUI.frame2.setVisible(true);
+                }
+            }
+            else{
+            GUI.frameVender.setVisible(false);
+            GUI.frame2.setVisible(true);
+            }
+            
+        }
+        
+        
+        });
+        
     
-    
-}
+}}
